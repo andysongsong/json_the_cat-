85d6c1731e57147daa70c690c10ca32a0051714d
andysongsong <sxj930916@gmail.com>
2021-07-30 05:39:18 +0000

    add test and refactor

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,16 +1,21 @@
-const breedname = process.argv.slice(2);
 const request = require("request");
 
-const url = `https://api.thecatapi.com/v1/breeds/search?q=${breedname}`;
-request(url, (error, response, body) => {
-  if (error) {
-    console.log("error:", error); // Print error if error
-  }
-  const data = JSON.parse(body);
-  if (data.length === 0) {
-    return console.log("The breed doesn't exist");
-  }
-  // console.log(data);
-  // console.log(typeof data);
-  console.log(data[0].description);
-});
+const fetchBreedDescription = (breedName, callback) => {
+  request(
+    `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`,
+    (error, response, body) => {
+      if (error) {
+        callback(`error`);
+        return;
+      }
+      const data = JSON.parse(body);
+      if (data.length === 0) {
+        callback("The breed doesn't exist");
+        return;
+      }
+
+      callback(error, data[0].description);
+    }
+  );
+};
+module.exports = { fetchBreedDescription };
